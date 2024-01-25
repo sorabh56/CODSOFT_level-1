0e095339613cdf530cacf4e30e545180ca403dc3
Sorabh Kumar <119025347+sorabh56@users.noreply.github.com>
2024-01-25 10:55:31 +0530

    Add files via upload
    Creating a personal portfolio using CSS and HTML is a popular beginner web development
    project.
    Header Section: Add your name or a logo at the top.
    Optionally, include a brief introduction or tagline.
    About Section: Insert an image of yourself.
    Write a short bio highlighting your skills and experience.
    Skills Section: List your key skills or areas of expertise.
    Projects Section: Showcase samples of your work or projects.
    Include project titles, descriptions, and images.
    Resume Section: Provide a link to download your resume in PDF format.
    Contact Section: Include your contact information, such as email address and phone
    number.
    Footer: Add a copyright notice and any additional links or information

---
+++ Portfoolio wdebsite/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("sorabh.tekt", {
+    strings: ["frontend ", "youtube", "web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000, // Adjust the backDelay according to your preference
+    loop: true
+});
